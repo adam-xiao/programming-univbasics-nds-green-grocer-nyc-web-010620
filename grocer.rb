773b2108dedeaea27051a79bd394653808d8e1d3
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-02 01:47:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,6 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 items_and_desc = cart[index]
-print cart[0]
 
 end
 

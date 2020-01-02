3bdc9a1d23078afaded77e69991c138d2c511861
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-02 01:44:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,8 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-print cart
+items_and_desc = cart[index]
+items_and_desc[:item].count
 
 end
 
